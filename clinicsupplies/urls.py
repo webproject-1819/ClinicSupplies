aef12e0544a47b6017f08c45d40ea3bee38125a1
juanjopdes <juanjopdes@gmail.com>
2019-04-11 17:02:25 +0200

    Templates working and linked.

--- clinicsupplies/urls.py
+++ clinicsupplies/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('', include('drugshop.urls')),
+    path('', include('drugshop.urls')),
 ]
