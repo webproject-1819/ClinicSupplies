5c4b76a17675953f4a83d31ef4808c766b603176
juanjopdes <juanjopdes@gmail.com>
2019-04-11 13:41:21 +0200

    all templates bases done and linked

--- drugshop/urls.py
+++ drugshop/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 
     url(r'^home/', home, name="home"),
     url(r'^catalogue/', catalogue, name="catalogue"),
-
+    url(r'^sales/', catalogue, name="sales"),
+    url(r'^information/', catalogue, name="information"),
+    url(r'^shopping_cart/', catalogue, name="shopping_cart"),
 ]
