ab443a8f4a7f3f3fd3048b2bdf11d625c13fe1be
nietzche <havocf4u@gmail.com>
2019-04-14 22:40:29 +0200

    Backend almost done, need to relate with frontend.

---
+++ drugshop/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from drugshop.models import product, stock
+
+
+class productForm(ModelForm):
+    class Meta:
+        model = product
+        fields = "__all__"
+        exclude = ('user',)
+
+class stockForm(ModelForm):
+    class Meta:
+        model = stock
+        fields = "__all__"
+        exclude = ('user',)
--- drugshop/models.py
+++ drugshop/models.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
 
 # Create your models here.
+from django.urls import reverse
 
 
 class product(models.Model):
@@ -10,14 +14,25 @@ class product(models.Model):
     reference = models.BigIntegerField(primary_key=True, default=00000000000)
     preu = models.FloatField(null=True)
     descripcio = models.CharField(max_length=255)
-    image = models.ImageField(upload_to = 'images/', default = 'images/None/no-img.jpg')
+    image = models.ImageField(upload_to='images/', default='images/None/no-img.jpg')
+
+    def averageRating(self):
+        reviewCount = self.product_review_set.count()
+        if not reviewCount:
+            return 0
+        else:
+            ratingSum = sum([float(review.rating) for review in self.product_review_set.all()])
+            return ratingSum / reviewCount
 
     def __unicode__(self):
         return u"%s" % self.name
 
+    def get_absolute_url(self):
+        return reverse('drugshop:product_detail', kwargs={'pk': self.pk})
+
 
 class stock(models.Model):
-    key = models.ForeignKey(product, on_delete=models.CASCADE, null=True)
+    key = models.ForeignKey(product, on_delete=models.CASCADE, null=True, unique=True)
     quantitiy = models.IntegerField(null=True)
 
     def available(self):
@@ -28,21 +43,28 @@ class stock(models.Model):
         return u"%s" % self.name
 
 
+class Category(models.Model):
+    CATEGORY_CHOICES = ((0, 'Sales'), (1, 'Outlete'), (2, 'New'), (3, 'Standard'))
+    category = models.PositiveSmallIntegerField('Category', blank=False, default=0, choices=CATEGORY_CHOICES)
+    user = models.ForeignKey(User, default=1)
+
+
+class Review(models.Model):
+    ''' Review atributes '''
+    RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'))
+    rating = models.PositiveSmallIntegerField('Rating (stars)', blank=False, default=3, choices=RATING_CHOICES)
+    comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+    date = models.DateField(default=date.today)
+
+
+class product_review(Review):
+    product = models.ForeignKey(product)
+
+
 class customer(models.Model):
     nom = models.CharField(max_length=255)
-    adressa_fisica = models.TextField()
+    adresa_fisica = models.CharField(max_length=255,default= 'Here')
     telefon = models.IntegerField()
     numero_compte = models.BigIntegerField()
-
-
-class carret(models.Model):
-    client = models.ForeignKey(customer,on_delete=models.CASCADE, null=True)
-    list = models.ForeignKey(product)
-
-    def final_price(self):
-        return self.list.aggregate('preu')
-
-
-
-
-
+    user = models.ForeignKey(User, default=1, primary_key=True)
