d7d7c419a45a607321e93660254dc7a79e8dca7f
aandreuu <andreunv98@gmail.com>
2019-05-08 16:10:07 +0200

    fixed urls

--- drugshop/urls.py
+++ drugshop/urls.py
@@ -7,10 +7,10 @@ from .views import *
 
 urlpatterns = [
 
-    url(r'^home', home, name='home'),
+    url(r'^home/', home, name='home'),
     url(r'^catalogue/$', views.productos, name='catalogue'),
-    url(r'^sales', sales, name='sales'),
-    url(r'^information', information, name='information'),
-    url(r'^shopping_cart', shopping_cart, name='shopping cart'),
+    url(r'^sales/', sales, name='sales'),
+    url(r'^information/', information, name='information'),
+    url(r'^shopping_cart/', shopping_cart, name='shopping cart'),
 
 ]
