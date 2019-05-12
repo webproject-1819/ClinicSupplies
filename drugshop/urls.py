88cf2d91b79fd49a6de2b6de556a7694fe1c0541
nietzche <havocf4u@gmail.com>
2019-05-12 21:53:39 +0200

    Minor improvements

--- drugshop/urls.py
+++ drugshop/urls.py
@@ -8,8 +8,8 @@ from .views import *
 
 urlpatterns = [
 
-    url(r'^', home, name='home'),
-    url(r'^catalogue/$', views.productos, name='catalogue'),
+    url(r'^home/$', home, name='home'),
+    url(r'^$', views.productos, name='catalogue'),
     url(r'^producte/(?P<reference>[-\w]+)$', views.producte_detail, name='producte'),
     url(r'^sales/$', views.sales, name='sales'),
     url(r'^product_delete/(?P<pk>\d+)/delete/$', views.producte_delete, name='product_delete'),
